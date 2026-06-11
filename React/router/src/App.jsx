3ff076fae2cafebbbd256dfcee99f715b58ca84a
Anandhakumar <anandhaanavamani@gmail.com>
2026-06-11 08:53:40 +0530

    adds

--- React/router/src/App.jsx
+++ React/router/src/App.jsx
@@ -5,8 +5,74 @@ import AllProducts from './Pages/AllProducts/AllProducts'
 import Mens from './Pages/Mens/Mens'
 import Womens from './Pages/Womens/Womens'
 import Kids from './Pages/Kids/Kids'
+import MensShirt from "./assets/Images/Shirt.jpg"
 
 const App = () => {
+  const Products = [
+    {
+      prodCat:"Mens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    },
+    {
+      prodCat:"Mens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    },
+    {
+      prodCat:"Mens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    },
+    {
+      prodCat:"Mens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    },
+    {
+      prodCat:"Womens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    }
+    ,{
+      prodCat:"Womens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    }
+    ,{
+      prodCat:"Womens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    },
+    {
+      prodCat:"Womens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    },
+    {
+      prodCat:"Womens",
+      prodName:"MensShirt",
+      prodPrice:300,
+      prodDesc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio laudantium dolorem ad saepe nesciunt labore veniam nihil aut. Atque magnam unde quos quaerat excepturi! Quia, ipsum dolorum. Quas, facere aliquid.",
+      image:MensShirt
+    }
+  ]
   return (
     <BrowserRouter>
       <div className='App'>
